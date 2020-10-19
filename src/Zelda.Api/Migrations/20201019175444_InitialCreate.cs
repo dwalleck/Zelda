d319b25fdfa0142d6cf9907914747548533424d4
Daryl Walleck <dwalleck@gmail.com>
2020-10-19 12:59:34 -0500

    Added migrations, fixed sln file

---
+++ src/Zelda.Api/Migrations/20201019175444_InitialCreate.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Zelda.Api.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Links",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Url = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Links", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tags",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tags", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LinkTag",
+                columns: table => new
+                {
+                    LinksId = table.Column<Guid>(type: "uuid", nullable: false),
+                    TagsId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LinkTag", x => new { x.LinksId, x.TagsId });
+                    table.ForeignKey(
+                        name: "FK_LinkTag_Links_LinksId",
+                        column: x => x.LinksId,
+                        principalTable: "Links",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_LinkTag_Tags_TagsId",
+                        column: x => x.TagsId,
+                        principalTable: "Tags",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LinkTag_TagsId",
+                table: "LinkTag",
+                column: "TagsId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LinkTag");
+
+            migrationBuilder.DropTable(
+                name: "Links");
+
+            migrationBuilder.DropTable(
+                name: "Tags");
+        }
+    }
+}
--- src/Zelda.Api/Startup.cs
+++ src/Zelda.Api/Startup.cs
@@ -36,7 +36,7 @@ namespace Zelda
             });
 
             services.AddDbContext<ZeldaContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("ZeldaContext")));
+                    options.UseNpgsql(Configuration["ConnectionStrings:ZeldaContext"]));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
